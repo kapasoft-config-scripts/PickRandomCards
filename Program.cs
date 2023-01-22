069c5ff8b5cdf857637c7cf6b46a6ced288c2aad
kapacma <mkapacs@gmail.com>
2023-01-22 20:08:59 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using PickRandomCards;
+
+Console.Write("Please, enter number of cards to pick: ");
+string line = Console.ReadLine();
+if(int.TryParse(line, out int numerOfCards))
+{
+    string[] cardsPicked = CardPicker.PickSomeCards(numerOfCards);
+    foreach(string card in cardsPicked)
+    {
+        Console.WriteLine(card);
+    }
+    
+    //Console.WriteLine(cardsPicked);
+}
+else
+{
+    Console.WriteLine("Input isn't number but expected one");
+}
